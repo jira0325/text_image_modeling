5772820808a7ea39dcab3a0a253885e97bad6ea8
jira0325 <hajarbenzekri@gmail.com>
2024-01-29 22:56:54 +0100

    edit build2

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,13 +3,13 @@
 # Script pour construire et pousser une image Docker
 
 if [ "$1" == "mysql" ]; then
-    docker build -t $DOCKER_USERNAME/mysql:latest ./mysql
+    docker build -t $DOCKER_USERNAME/mysql:latest mysql
     docker push $DOCKER_USERNAME/mysql:latest
 elif [ "$1" == "api" ]; then
-    docker build -t $DOCKER_USERNAME/api:latest ./api
+    docker build -t $DOCKER_USERNAME/api:latest api
     docker push $DOCKER_USERNAME/api:latest
 elif [ "$1" == "model" ]; then
-    docker build -t $DOCKER_USERNAME/model:latest ./model
+    docker build -t $DOCKER_USERNAME/model:latest model
     docker push $DOCKER_USERNAME/model:latest
 else
     echo "Usage: $0 [mysql|api|model]"
