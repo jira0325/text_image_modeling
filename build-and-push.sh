2862a8621671d3aa4704e6781a811ebdf4fc6618
jira0325 <hajarbenzekri@gmail.com>
2024-02-23 14:03:07 +0100

    image streamlit

--- build-and-push.sh
+++ build-and-push.sh
@@ -13,9 +13,13 @@ elif [ "$1" == "api" ]; then
 elif [ "$1" == "model" ]; then
     docker build -t $DOCKER_USERNAME/model:latest ./model
     docker push $DOCKER_USERNAME/model:latest
+elif [ "$1" == "streamlit" ]; then
+    docker build -t $DOCKER_USERNAME/streamlit:latest ./streamlit
+    docker push $DOCKER_USERNAME/streamlit:latest    
 else
-    echo "Usage: $0 [mysql|api|model]"
+    echo "Usage: $0 [mysql|api|model|streamlit]"
     exit 1
 fi
+
 
 echo "Build and push completed."
