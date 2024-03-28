b0088b1cc20bc8131af10e33bc39c048f65469af
jira0325 <jira0325@users.noreply.github.com>
2024-03-28 13:53:26 +0000

    deploy: 3da80f8afb85711660095fa14640cce0c5ed84cf

--- application_sreamlit.py
+++ application_sreamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # URL de votre API FastAPI
-API_URL = "http://localhost:8000"
+API_URL = "https://textimagemodeling-wdnvphwmkxjxvyfr8rxpwh.streamlit.app/"
 
 # Variable globale pour stocker le texte extrait
 extracted_text = None
